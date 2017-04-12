ce8cf7530738ac786136a7410a453d9929df9390
wolter <alosina35@gmail.com>
2017-04-12 15:47:57 +0000

    Update Info.lua

--- plugins/Info.lua
+++ plugins/Info.lua
@@ -49,7 +49,7 @@ local function info_cb(arg, data)
   local um_hash = 'msgs:'..data.id_..':'..arg.chat_id
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'Total messages : '..user_info_msgs..'\n\n'
-  text = text..'@BeyondTeam'
+  text = text..'@wolterr'
   tdcli.sendMessage(arg.chat_id, arg.msgid, 0, text, 0, "md")
 end
 tdcli_function ({
