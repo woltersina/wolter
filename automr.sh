61743e9bfc0c3a00b473de530aeb1415ecc00b18
wolter <alosina35@gmail.com>
2017-04-12 22:34:08 +0430

    Add files via upload

---
+++ automr.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+COUNTER=1
+while(true) do
+./wolter.sh
+curl "https://api.telegram.org/bot249864339:AAHTcvdCXKP0zwuB5NGvh_KyM_QdRZklNTU/sendmessage" -F "chat_id=127165321" -F "text=#wolter-#TeaM-Reloaded-${COUNTER}-times"
+let COUNTER=COUNTER+1 
+done
