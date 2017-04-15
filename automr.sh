2a746f1a1f9ed5bf9a4fe2618f44498c5fad96d8
wolter <alosina35@gmail.com>
2017-04-15 15:07:44 +0000

    Update automr.sh

--- automr.sh
+++ automr.sh
@@ -2,6 +2,6 @@
 COUNTER=1
 while(true) do
 ./wolter.sh
-curl "https://api.telegram.org/bot249864339:AHTcvdCXKP0zwuB5NGvh_KyM_QdRZklNTU/sendmessage" -F "chat_id=127165321" -F "text=#wolter-#TeaM-Reloaded-${COUNTER}-times"
+curl "https://api.telegram.org/bot249864339:AHTcvdCXKP0zwuB5NGvh_KyM_QdRZklNTU/sendmessage" -F "chat_id=127165321" -F "text=#wolter-#relunch-Reloaded-${COUNTER}-times"
 let COUNTER=COUNTER+1 
 done
