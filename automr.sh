9b6e52221a9eeb8ac73fff6b397afe9bd830f1f6
wolter <alosina35@gmail.com>
2017-04-29 07:45:22 +0000

    Delete automr.sh

--- automr.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-COUNTER=1
-while(true) do
-./wolter.sh
-curl "https://api.telegram.org/bot249864339:AHTcvdCXKP0zwuB5NGvh_KyM_QdRZklNTU/sendmessage" -F "chat_id=127165321" -F "text=#wolter-#TeaM-Reloaded-${COUNTER}-times"
-let COUNTER=COUNTER+1 
-done
